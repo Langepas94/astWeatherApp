d637252b5a3b6b14e47e5af89344535ee467cf29
Artem <artyom9494@yandex.ru>
2023-11-01 22:47:35 +0300

    remove protocol from locationManager

--- astWeatherApp/Workers/LocationWorker.swift
+++ astWeatherApp/Workers/LocationWorker.swift
@@ -9,7 +9,7 @@ import Foundation
 import CoreLocation
 import Combine
 
-class LocationWorker: NSObject, ObservableObject {
+class LocationWorker: NSObject {
     
     private let locationManager: CLLocationManager = {
         let location = CLLocationManager()
